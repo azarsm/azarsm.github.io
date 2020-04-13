110eae8484756b4e0477100c96a250477a3488f4
Aaron Ram <aaronram@192-168-1-105.tpgi.com.au>
2020-04-13 20:01:39 +1000

    Created main.js

---
+++ main.js
@@ -0,0 +1,100 @@
+
+function sList( lu )
+{
+var sList = [];
+var hc = 4;
+
+
+sList [0] = [{r:"Mk 16:15-20" , vr:"NET" , v:'16:15 He said to them, “Go into all the world and preach the gospel to every creature. 16 The one who believes and is baptized will be saved, but the one who does not believe will be condemned. 17 These signs will accompany those who believe: In my name they will drive out demons; they will speak in new languages; 18 they will pick up snakes with their hands, and whatever poison they drink will not harm them; they will place their hands on the sick and they will be well.” 19 After the Lord Jesus had spoken to them, he was taken up into heaven and sat down at the right hand of God. 20 They went out and proclaimed everywhere, while the Lord worked with them and confirmed the word through the accompanying signs. '}]; 
+sList [1] = [{r:"Ep 2:10" , vr:"NET" , v:'2:10 For we are his creative work, having been created in Christ Jesus for good works that God prepared beforehand so we can do them. '}]; 
+sList [2] = [{r:"Jn 15:16" , vr:"NET" , v:'15:16 You did not choose me, but I chose you and appointed you to go and bear fruit, fruit that remains, so that whatever you ask the Father in my name he will give you. '}]; 
+sList [3] = [{r:"2 Cor 3:6" , vr:"NET" , v:'3:6 who made us adequate to be servants of a new covenant not based on the letter but on the Spirit, for the letter kills, but the Spirit gives life. '}]; 
+sList [4] = [{r:"2 Cor 4:6" , vr:"NET" , v:'4:6 For God, who said “Let light shine out of darkness,” is the one who shined in our hearts to give us the light of the glorious knowledge of God in the face of Christ. '}]; 
+sList [5] = [{r:"2 Cor 5:18" , vr:"NET" , v:'5:18 And all these things are from God who reconciled us to himself through Christ, and who has given us the ministry of reconciliation. '}]; 
+sList [6] = [{r:"2 Cor 5:19" , vr:"NET" , v:'5:19 In other words, in Christ God was reconciling the world to himself, not counting people’s trespasses against them, and he has given us the message of reconciliation. '}]; 
+sList [7] = [{r:"Ep 4:12" , vr:"NET" , v:'4:12 to equip the saints for the work of ministry, that is, to build up the body of Christ, '}]; 
+sList [8] = [{r:"Ps 32:8" , vr:"NET" , v:'32:8 I will instruct and teach you about how you should live. I will advise you as I look you in the eye. '}]; 
+sList [9] = [{r:"Jn 14:26" , vr:"NET" , v:'14:26 But the Advocate, the Holy Spirit, whom the Father will send in my name, will teach you everything, and will cause you to remember everything I said to you. '}]; 
+sList [10] = [{r:"1 Jn 2:27" , vr:"NET" , v:'2:27 Now as for you, the anointing that you received from him resides in you, and you have no need for anyone to teach you. But as his anointing teaches you about all things, it is true and is not a lie. Just as it has taught you, you reside in him. '}]; 
+sList [11] = [{r:"2 Ti 3:16" , vr:"NET" , v:'3:16 Every scripture is inspired by God and useful for teaching, for reproof, for correction, and for training in righteousness, '}]; 
+sList [12] = [{r:"2 Ti 2:15" , vr:"NET" , v:'2:15 Make every effort to present yourself before God as a proven worker who does not need to be ashamed, teaching the message of truth accurately. '}]; 
+sList [13] = [{r:"Col 4:17" , vr:"NET" , v:'4:17 And tell Archippus, “See to it that you complete the ministry you received in the Lord.” '}]; 
+sList [14] = [{r:"2 Ti 4:2" , vr:"NET" , v:'4:2 Preach the message, be ready whether it is convenient or not, reprove, rebuke, exhort with complete patience and instruction. '}]; 
+sList [15] = [{r:"1 Cor 12:7" , vr:"NET" , v:'12:7 To each person the manifestation of the Spirit is given for the benefit of all. '}]; 
+sList [16] = [{r:"2 Ti 2:24" , vr:"NET" , v:'2:24 And the Lord’s slave must not engage in heated disputes but be kind toward all, an apt teacher, patient, '}]; 
+sList [17] = [{r:"2 Ti 2:25" , vr:"NET" , v:'2:25 correcting opponents with gentleness. Perhaps God will grant them repentance and then knowledge of the truth '}]; 
+sList [18] = [{r:"1 Cor 10:32-33" , vr:"NET" , v:'10:32 Do not give offense to Jews or Greeks or to the church of God, 33 just as I also try to please everyone in all things. I do not seek my own benefit, but the benefit of many, so that they may be saved. '}]; 
+sList [19] = [{r:"Phil 2:7" , vr:"NET" , v:'2:7 but emptied himselfby taking on the form of a slave, by looking like other men, and by sharing in human nature. '}]; 
+sList [20] = [{r:"1 Th 4:3-4" , vr:"NET" , v:'4:3 For this is God’s will: that you become holy, that you keep away from sexual immorality, 4 that each of you know how to possess his own body in holiness and honor, '}]; 
+sList [21] = [{r:"Ep 5:4" , vr:"NET" , v:'5:4 Neither should there be vulgar speech, foolish talk, or coarse jesting – all of which are out of character – but rather thanksgiving. '}]; 
+sList [22] = [{r:"Ep 6:18-19" , vr:"NET" , v:'6:18 With every prayer and petition, pray at all times in the Spirit, and to this end be alert, with all perseverance and petitions for all the saints. 19 Pray for me also, that I may be given the right words when I begin to speak – that I may confidently make known the mystery of the gospel, '}]; 
+sList [23] = [{r:"Phil 1:6" , vr:"NET" , v:'1:6 For I am sure of this very thing, that the one who began a good work in you will perfect it until the day of Christ Jesus. '}]; 
+sList [24] = [{r:"Col 4:5" , vr:"NET" , v:'4:5 Conduct yourselves with wisdom toward outsiders, making the most of the opportunities. '}]; 
+sList [25] = [{r:"Col 4:6" , vr:"NET" , v:'4:6 Let your speech always be gracious, seasoned with salt, so that you may know how you should answer everyone. '}]; 
+sList [26] = [{r:"1 Pe 4:10" , vr:"NET" , v:'4:10 Just as each one has received a gift, use it to serve one another as good stewards of the varied grace of God. '}]; 
+sList [27] = [{r:"Ga 5:22-23" , vr:"NET" , v:'5:22 But the fruit of the Spirit is love, joy, peace, patience, kindness, goodness, faithfulness, 23 gentleness, and self-control. Against such things there is no law. '}]; 
+sList [28] = [{r:"Ga 5:24" , vr:"NET" , v:'5:24 Now those who belong to Christ have crucified the flesh with its passions and desires. '}]; 
+sList [29] = [{r:"1 Cor 3:13-15" , vr:"NET" , v:'3:13 each builder’s work will be plainly seen, for the Day will make it clear, because it will be revealed by fire. And the fire will test what kind of work each has done. 14 If what someone has built survives, he will receive a reward. 15 If someone’s work is burned up, he will suffer loss. He himself will be saved, but only as through fire. '}]; 
+sList [30] = [{r:"Dt 6:5-7" , vr:"NET" , v:'6:5 You must love the Lord your God with your whole mind, your whole being, and all your strength. 6 These words I am commanding you today must be kept in mind, 7 and you must teach them to your children and speak of them as you sit in your house, as you walk along the road, as you lie down, and as you get up. '}]; 
+sList [31] = [{r:"Jas 4:10" , vr:"NET" , v:'4:10 Humble yourselves before the Lord and he will exalt you. '}]; 
+sList [32] = [{r:"Col 4:2" , vr:"NET" , v:'4:2 Be devoted to prayer, keeping alert in it with thanksgiving. '}]; 
+sList [33] = [{r:"1 Ch 28:20" , vr:"NET" , v:'28:20 David said to his son Solomon: “Be strong and brave! Do it! Don’t be afraid and don’t panic! For the Lord God, my God, is with you. He will not leave you or abandon you before all the work for the service of the Lord’s temple is finished. '}]; 
+sList [34] = [{r:"Col 4:3" , vr:"NET" , v:'4:3 At the same time pray for us too, that God may open a door for the message so that we may proclaim the mystery of Christ, for which I am in chains. '}]; 
+sList [35] = [{r:"Ps 107:2" , vr:"NET" , v:'107:2 Let those delivered by the Lord speak out, those whom he delivered from the power of the enemy, '}]; 
+sList [36] = [{r:"Ep 1:7" , vr:"NET" , v:'1:7 In him we have redemption through his blood, the forgiveness of our offenses, according to the riches of his grace '}]; 
+sList [37] = [{r:"1 Jn 1:7" , vr:"NET" , v:'1:7 But if we walk in the light as he himself is in the light, we have fellowship with one another and the blood of Jesus his Son cleanses us from all sin. '}]; 
+sList [38] = [{r:"Ro 5:9" , vr:"NET" , v:'5:9 Much more then, because we have now been declared righteous by his blood, we will be saved through him from God’s wrath. '}]; 
+sList [39] = [{r:"Heb 13:12" , vr:"NET" , v:'13:12 Therefore, to sanctify the people by his own blood, Jesus also suffered outside the camp. '}]; 
+sList [40] = [{r:"1 Cor 6:19-20" , vr:"NET" , v:'6:19 Or do you not know that your body is the temple of the Holy Spirit who is in you, whom you have from God, and you are not your own? 20 For you were bought at a price. Therefore glorify God with your body. '}]; 
+sList [41] = [{r:"Re 12:11" , vr:"NET" , v:'12:11 But they overcame him by the blood of the Lamband by the word of their testimony,and they did not love their lives so much that they were afraid to die. '}]; 
+sList [42] = [{r:"Lk 10:19" , vr:"NET" , v:'10:19 Look, I have given you authority to tread on snakes and scorpions and on the full force of the enemy, and nothing will hurt you. '}]; 
+sList [43] = [{r:"Lk 10:18" , vr:"NET" , v:'10:18 So he said to them, “I saw Satan fall like lightning from heaven. '}]; 
+sList [44] = [{r:"Ge 12:3" , vr:"NET" , v:'12:3 I will bless those who bless you, but the one who treats you lightly I must curse, so that all the families of the earth may receive blessing through you.” '}]; 
+sList [45] = [{r:"Mt 20:28" , vr:"NET" , v:'20:28 just as the Son of Man did not come to be served but to serve, and to give his life as a ransom for many.” '}]; 
+sList [46] = [{r:"Ga 3:9" , vr:"NET" , v:'3:9 So then those who believe are blessed along with Abraham the believer. '}]; 
+sList [47] = [{r:"Ga 3:29" , vr:"NET" , v:'3:29 And if you belong to Christ, then you are Abraham’s descendants, heirs according to the promise. '}]; 
+sList [48] = [{r:"Heb 11:1" , vr:"NET" , v:'11:1 Now faith is being sure of what we hope for, being convinced of what we do not see. '}]; 
+sList [49] = [{r:"Heb 11:3" , vr:"NET" , v:'11:3 By faith we understand that the worlds were set in order at God’s command, so that the visible has its origin in the invisible. '}]; 
+sList [50] = [{r:"Heb 11:6" , vr:"NET" , v:'11:6 Now without faith it is impossible to please him, for the one who approaches God must believe that he exists and that he rewards those who seek him. '}]; 
+sList [51] = [{r:"Phil 3:9" , vr:"NET" , v:'3:9 and be found in him, not because I have my own righteousness derived from the law, but because I have the righteousness that comes by way of Christ’s faithfulness – a righteousness from God that is in fact based on Christ’s faithfulness. '}]; 
+sList [52] = [{r:"Hab 2:4" , vr:"NET" , v:'2:4 Look, the one whose desires are not upright will faint from exhaustion, but the person of integrity will live because of his faithfulness. '}]; 
+sList [53] = [{r:"Ge 15:6" , vr:"NET" , v:'15:6 Abram believed the Lord, and the Lord credited it as righteousness to him. '}]; 
+sList [54] = [{r:"1 Ti 1:19" , vr:"NET" , v:'1:19 To do this you must hold firmly to faith and a good conscience, which some have rejected and so have suffered shipwreck in regard to the faith. '}]; 
+sList [55] = [{r:"1 Ti 1:12" , vr:"NET" , v:'1:12 I am grateful to the one who has strengthened me, Christ Jesus our Lord, because he considered me faithful in putting me into ministry, '}]; 
+sList [56] = [{r:"Jude 1:20" , vr:"NET" , v:'1:20 But you, dear friends, by building yourselves up in your most holy faith, by praying in the Holy Spirit, '}]; 
+sList [57] = [{r:"2 Cor 4:18" , vr:"NET" , v:'4:18 because we are not looking at what can be seen but at what cannot be seen. For what can be seen is temporary, but what cannot be seen is eternal. '}]; 
+sList [58] = [{r:"Heb 12:2" , vr:"NET" , v:'12:2 keeping our eyes fixed on Jesus, the pioneer and perfecter of our faith. For the joy set out for him he endured the cross, disregarding its shame, and has taken his seat at the right hand of the throne of God. '}]; 
+sList [59] = [{r:"Mt 17:20" , vr:"NET" , v:'17:20 He told them, “It was because of your little faith. I tell you the truth, if you have faith the size of a mustard seed, you will say to this mountain, ‘Move from here to there,’ and it will move; nothing will be impossible for you.” '}]; 
+sList [60] = [{r:"Ro 10:17" , vr:"NET" , v:'10:17 Consequently faith comes from what is heard, and what is heard comes through the preached word of Christ. '}]; 
+sList [61] = [{r:"Lk 17:5" , vr:"NET" , v:'17:5 The apostles said to the Lord, “Increase our faith!” '}]; 
+sList [62] = [{r:"Mt 14:31" , vr:"NET" , v:'14:31 Immediately Jesus reached out his hand and caught him, saying to him, “You of little faith, why did you doubt?” '}]; 
+sList [63] = [{r:"Lk 9:1" , vr:"NET" , v:'9:1 After Jesus called the twelve together, he gave them power and authority over all demons and to cure diseases, '}]; 
+sList [64] = [{r:"Mt 10:1" , vr:"NET" , v:'10:1 Jesus called his twelve disciples and gave them authority over unclean spirits so they could cast them out and heal every kind of disease and sickness. '}]; 
+sList [65] = [{r:"Mt 10:8" , vr:"NET" , v:'10:8 Heal the sick, raise the dead, cleanse lepers, cast out demons. Freely you received, freely give. '}]; 
+sList [66] = [{r:"Jn 16:23" , vr:"NET" , v:'16:23 At that time you will ask me nothing. I tell you the solemn truth, whatever you ask the Father in my name he will give you. '}]; 
+sList [67] = [{r:"1 Jn 3:8" , vr:"NET" , v:'3:8 The one who practices sin is of the devil, because the devil has been sinning from the beginning. For this purpose the Son of God was revealed: to destroy the works of the devil. '}]; 
+sList [68] = [{r:"1 Jn 4:2" , vr:"NET" , v:'4:2 By this you know the Spirit of God: Every spirit that confesses Jesus as the Christ who has come in the flesh is from God, '}]; 
+sList [69] = [{r:"1 Cor 12:8-10" , vr:"NET" , v:'12:8 For one person is given through the Spirit the message of wisdom, and another the message of knowledge according to the same Spirit, 9 to another faith by the same Spirit, and to another gifts of healing by the one Spirit, 10 to another performance of miracles, to another prophecy, and to another discernment of spirits, to another different kinds of tongues, and to another the interpretation of tongues. '}]; 
+sList [70] = [{r:"1 Sa 17:47" , vr:"NET" , v:'17:47 and all this assembly will know that it is not by sword or spear that the Lord saves! For the battle is the Lord’s, and he will deliver you into our hand.” '}]; 
+sList [71] = [{r:"2 Cor 10:4" , vr:"NET" , v:'10:4 for the weapons of our warfare are not human weapons, but are made powerful by God for tearing down strongholds. We tear down arguments '}]; 
+sList [72] = [{r:"Ro 8:31" , vr:"NET" , v:'8:31 What then shall we say about these things? If God is for us, who can be against us? '}]; 
+sList [73] = [{r:"1 Jn 4:6" , vr:"NET" , v:'4:6 We are from God; the person who knows God listens to us, but whoever is not from God does not listen to us. By this we know the Spirit of truth and the spirit of deceit. '}]; 
+sList [74] = [{r:"Ep 6:17" , vr:"NET" , v:'6:17 And take the helmet of salvation and the sword of the Spirit (which is the word of God). '}]; 
+sList [75] = [{r:"Mt 12:28" , vr:"NET" , v:'12:28 But if I cast out demons by the Spirit of God, then the kingdom of God has already overtaken you. '}]; 
+sList [76] = [{r:"Mt 16:19" , vr:"NET" , v:'16:19 I will give you the keys of the kingdom of heaven. Whatever you bind on earth will have been bound in heaven, and whatever you release on earth will have been released in heaven.” '}]; 
+sList [77] = [{r:"Mt 12:29" , vr:"NET" , v:'12:29 How else can someone enter a strong man’s house and steal his property, unless he first ties up the strong man? Then he can thoroughly plunder the house. '}]; 
+sList [78] = [{r:"Mk 9:25" , vr:"NET" , v:'9:25 Now when Jesus saw that a crowd was quickly gathering, he rebuked the unclean spirit, saying to it, “Mute and deaf spirit, I command you, come out of him and never enter him again.” '}]; 
+sList [79] = [{r:"Ro 13:12" , vr:"NET" , v:'13:12 The night has advanced toward dawn; the day is near. So then we must lay aside the works of darkness, and put on the weapons of light. '}]; 
+sList [80] = [{r:"2 Cor 6:7" , vr:"NET" , v:'6:7 by truthful teaching, by the power of God, with weapons of righteousness both for the right hand and for the left, '}]; 
+sList [81] = [{r:"Ep 6:13" , vr:"NET" , v:'6:13 For this reason, take up the full armor of God so that you may be able to stand your ground on the evil day, and having done everything, to stand. '}]; 
+sList [82] = [{r:"Ep 6:14-15" , vr:"NET" , v:'6:14 Stand firm therefore, by fastening the belt of truth around your waist, by putting on the breastplate of righteousness, 15 by fitting your feet with the preparation that comes from the good news of peace, '}]; 
+sList [83] = [{r:"Ep 6:16-17" , vr:"NET" , v:'6:16 and in all of this, by taking up the shield of faith with which you can extinguish all the flaming arrows of the evil one. 17 And take the helmet of salvation and the sword of the Spirit (which is the word of God). '}]; 
+
+
+//lu= Math.floor(Math.random() * 83);  
+
+return sList[lu] ;
+
+//return sList[hc] ;
+
+}
